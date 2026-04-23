82a8a826a08ad00d08a1a6178d38775a53381821
Monandszy <monandszy@tuta.io>
2026-04-23 19:35:22 +0200

    fix: formatter typo

--- backend/transcend/src/main/java/code/bootstrap/ValidProfileInitializer.java
+++ backend/transcend/src/main/java/code/bootstrap/ValidProfileInitializer.java
@@ -1,23 +1,22 @@
 package code.bootstrap;
 
 import code.bootstrap.exceptions.IllegalProfileException;
+import java.util.List;
 import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 
-import java.util.List;
-
-public class ValidProfileInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+public class ValidProfileInitializer
+    implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
   @Override
   public void initialize(ConfigurableApplicationContext context) {
 
     ConfigurableEnvironment env = context.getEnvironment();
 
-    String[] allowed = Binder.get(env)
-        .bind("spring.allowed-profiles", String[].class)
-        .orElse(new String[0]);
+    String[] allowed =
+        Binder.get(env).bind("spring.allowed-profiles", String[].class).orElse(new String[0]);
     List<String> allowedList = List.of(allowed);
     List<String> activeList = List.of(env.getActiveProfiles());
 
