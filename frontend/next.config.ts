c7cbbf9a44ce1c3a40197c2045e9e609e0cf52ca
Fistxszek <kacper.orzechowski.04@gmail.com>
2026-05-06 17:43:07 +0200

    feat: frontend login integrated with endpoint

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        // When I fetch something starting with /api/
+        source: "/api/:path*",
+        // Send it to my backend on 8080
+        destination: "http://localhost:8080/:path*",
+      },
+    ];
+  },
   reactCompiler: true,
 };
 
