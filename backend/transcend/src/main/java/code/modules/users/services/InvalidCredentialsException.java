614823b0c88771fd14d12ff20d4dbf908dc466cc
Monandszy <monandszy@tuta.io>
2026-05-07 17:17:04 +0200

    ref: package by layer residue

--- backend/transcend/src/main/java/code/modules/users/services/InvalidCredentialsException.java
+++
@@ -1,8 +0,0 @@
-package code.modules.users.services;
-
-public class InvalidCredentialsException extends RuntimeException {
-
-  public InvalidCredentialsException() {
-    super("Invalid email or password");
-  }
-}
