d5f7bd9d8e0f05a858017a6480014af703636ff7
kinga <kinga.kwasniak5@gmail.com>
2026-05-27 16:14:17 +0200

    feat: package-info modulith docs

--- backend/transcend/src/main/java/code/chat/logic/StartChat.java
+++ backend/transcend/src/main/java/code/chat/logic/StartChat.java
@@ -1,7 +1,9 @@
 package code.chat.logic;
 
 import code.chat.ports.in.StartChatUseCase;
+import org.springframework.stereotype.Service;
 
+@Service
 public class StartChat implements StartChatUseCase {
     @Override
     public void startChat(StartChatCommand command) {
