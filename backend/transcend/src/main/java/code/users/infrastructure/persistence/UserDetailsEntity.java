4995270d40de4bf567eb7aaa61961a390a3ff620
Alina Ralcewicz <ralcevichalina70@gmail.com>
2026-06-01 11:13:24 +0200

    feat(db): Create UserDetails, Avatar Entities and database tables.

---
+++ backend/transcend/src/main/java/code/users/infrastructure/persistence/UserDetailsEntity.java
@@ -0,0 +1,21 @@
+package code.users.infrastructure.persistence;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user_details")
+public class UserDetailsEntity {
+
+    @EmbeddedId @EqualsAndHashCode.Include private UserIdEntity id;
+
+    @Column(name = "display_name")
+    private String displayName;
+
+    @Column(name = "avatar_url", nullable = false)
+    private String avatarUrl;
+}
