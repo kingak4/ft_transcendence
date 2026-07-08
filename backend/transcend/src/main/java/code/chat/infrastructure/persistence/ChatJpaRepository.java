7191753c80792bc348a0a445dd720da34a3fef07
Alina Ralcewicz <ralcevichalina70@gmail.com>
2026-07-08 15:25:11 +0200

    feat(chat-persistence): implement persistence layer for chat model

--- backend/transcend/src/main/java/code/chat/infrastructure/persistence/ChatJpaRepository.java
+++ backend/transcend/src/main/java/code/chat/infrastructure/persistence/ChatJpaRepository.java
@@ -1,4 +1,24 @@
 package code.chat.infrastructure.persistence;
 
-public class ChatJpaRepository {
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface ChatJpaRepository extends JpaRepository<ChatEntity, ChatIdEntity> {
+    @Query("select distinct c from ChatEntity c join c.participants p where p = :userId")
+    Page<ChatEntity> findAllByParticipant(@Param("userId") UUID userId, Pageable pageable);
+
+    @Query(
+            "select c from ChatEntity c "
+                    + "where size(c.participants) = 2 "
+                    + "and :initiator member of c.participants "
+                    + "and :recipient member of c.participants")
+    Optional<ChatEntity> findByParticipants(
+            @Param("initiator") UUID initiator, @Param("recipient") UUID recipient);
 }
--- backend/transcend/src/main/java/code/chat/infrastructure/persistence/MessageEntity.java
+++ backend/transcend/src/main/java/code/chat/infrastructure/persistence/MessageEntity.java
@@ -1,4 +1,34 @@
 package code.chat.infrastructure.persistence;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "messages")
 public class MessageEntity {
+    @EmbeddedId
+    @EqualsAndHashCode.Include MessageIdEntity id;
+
+    @Embedded
+    @AttributeOverride(name = "val", column = @Column(name = "chat_id", nullable = false))
+    private ChatIdEntity chatId;
+
+    @Column(name = "sender_id", nullable = false)
+    private UUID senderId;
+
+    @Column(name = "content", nullable = false, columnDefinition = "text")
+    private String content;
+
+    @Column(name = "created_at", nullable = false)
+    private OffsetDateTime createdAt;
 }
