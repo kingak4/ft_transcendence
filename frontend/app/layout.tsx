c55f71300c2cdce81eecf2388698352310ea93d1
Fistxszek <kacper.orzechowski.04@gmail.com>
2026-07-22 13:44:56 +0200

    fix: preload warning fix

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -6,11 +6,13 @@ import StompProvider from './components/StompProvider';
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
+  preload: false,
 });
 
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
+  preload: false,
 });
 
 export const metadata: Metadata = {
