7061a4099d9f72a0c2531d9c3bd478a4a489550f
Fistxszek <kacper.orzechowski.04@gmail.com>
2026-08-07 21:08:45 +0200

    fix: comments fixes

--- frontend/app/(app)/layout.tsx
+++ frontend/app/(app)/layout.tsx
@@ -1,7 +1,6 @@
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
-import { client } from '../lib/api-clients';
 import { assertValidSession } from '../lib/session';
 
 import Footer from '../components/Footer';
